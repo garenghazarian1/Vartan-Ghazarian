191e35b4c6b67155edd4d2f2a14514e5f781b698
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-11-17 17:33:00 +0100

    module js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const items = document.querySelectorAll(".carousel-item");
-  let counter = 0;
+import { initializeCarousel } from "./carousel.js";
 
-  // Function to change to the next background image
-  const changeImage = () => {
-    // Remove the "active" class from all items
-    items.forEach((item) => {
-      item.classList.remove("active");
-    });
-
-    // Increment counter and reset if it reaches the end
-    counter = (counter + 1) % items.length;
-
-    // Add the "active" class to the current item
-    items[counter].classList.add("active");
-  };
-
-  // Move to the previous background image when "Prev" button is clicked
-  document.getElementById("prev").addEventListener("click", () => {
-    items[counter].classList.remove("active");
-    counter = (counter - 1 + items.length) % items.length; // Move backwards
-    items[counter].classList.add("active");
-  });
-
-  // Move to the next background image when "Next" button is clicked
-  document.getElementById("next").addEventListener("click", changeImage);
-
-  // Automatically change images every 3 seconds
-  setInterval(changeImage, 5000);
-});
+initializeCarousel();
