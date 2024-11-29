21f75cdf5b20518626058b4e1bef86cbd9fea250
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-11-29 22:32:57 +0100

    edit some stylings color

--- assets/js/sideBar.js
+++ assets/js/sideBar.js
@@ -6,11 +6,11 @@ export function toggleSidebar() {
 
   // Check if the sidebar is now open and adjust styles accordingly
   if (sidebar.classList.contains("open")) {
-    sidebar.style.left = "0px"; // Move sidebar into view
+    sidebar.style.left = "0rem"; // Move sidebar into view
     hamburger.style.opacity = "0"; // Hide hamburger icon
     hamburger.style.pointerEvents = "none"; // Disable interactions
   } else {
-    sidebar.style.left = "-250px"; // Move sidebar out of view
+    sidebar.style.left = "-15.625rem"; // Move sidebar out of view (250px/16px = 15.625rem)
     hamburger.style.opacity = "1"; // Make hamburger icon visible
     hamburger.style.pointerEvents = "auto"; // Enable interactions
   }
@@ -21,7 +21,7 @@ export function closeSidebar() {
   const sidebar = document.querySelector(".sidebar");
   const hamburger = document.querySelector(".hamburger");
   sidebar.classList.remove("open");
-  sidebar.style.left = "-250px"; // Ensure the sidebar is moved out of view
+  sidebar.style.left = "-15.625rem"; // Ensure the sidebar is moved out of view
   hamburger.style.opacity = "1"; // Ensure hamburger is visible when sidebar is closed
   hamburger.style.pointerEvents = "auto"; // Ensure interactions are enabled
 }
