f754962ffadd0a5c661850bda0a916ee49a3722f
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-11-29 20:36:18 +0100

    some html structure for youtube part edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 import { initializeCarousel } from "./carousel.js";
-// import { loadVideoSection } from "./youtube.js";
+import { loadVideoSection } from "./youtubeHtml.js";
 import { setupAudioPlayers } from "./audioManager.js";
 
 initializeCarousel();
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   loadVideoSection();
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  loadVideoSection();
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   // Setup the audio players once the DOM is fully loaded
